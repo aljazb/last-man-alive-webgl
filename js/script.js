fc3280e5ad198d641fbf36598ae2b63af71ffc1f
markozeman <mz4730@student.uni-lj.si>
2016-11-09 21:39:06 +0000

    Shorter and more structured code

--- js/script.js
+++ js/script.js
@@ -1,82 +1,74 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var canvas = document.getElementById('canvas');
-    var engine = new BABYLON.Engine(canvas, true);
-    var character;
-    var charMoveSpeed = 0.1;
-    
-    var createScene = function() {
-        var scene = new BABYLON.Scene(engine);
-        var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 20,-15), scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
-        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-        character = BABYLON.Mesh.CreateSphere('sphere1', 16, 1, scene);
-        character.position.y = 1;
-        var ground = BABYLON.Mesh.CreateGround('ground1', 25, 18, 2, scene);
-    
-        return scene;
-    }
-    
-    var scene = createScene();
+window.addEventListener('DOMContentLoaded', main);
+
+var keys = { letft:false, right:false, up:false, down:false };
+var engine, scene, character;
+var charMoveSpeed = 0.1;
+
+
+function main(){
+    var engine_and_scene = createScene();
+    engine = engine_and_scene[0];
+    scene = engine_and_scene[1];
     
     engine.runRenderLoop(function() {
         scene.render();
     });
 
-    window.addEventListener('resize', function() {
-        engine.resize();
-    });
+    window.addEventListener('resize', function() { engine.resize(); });
     
-    var keys = { letft:false, right:false, up:false, down:false };
+    window.addEventListener("keydown", function() { key_up_or_down(event, true); });
+    window.addEventListener("keyup", function() { key_up_or_down(event, false); });
     
-    window.addEventListener("keydown", onKeyDown, false);
-    window.addEventListener("keyup", onKeyUp, false);
-    
-    function onKeyDown(event) {    
-        if (event.keyCode == 65 || event.keyCode == 37) {  
-            keys.left = true;
-        }
-        if (event.keyCode == 68 || event.keyCode == 39) { 
-            keys.right = true;    
-        }
-        if (event.keyCode == 87 || event.keyCode == 38) {
-            keys.up = true;  
-        }
-        if (event.keyCode == 83 || event.keyCode == 40) { 
-            keys.down = true;
-        } 
+    engine.runRenderLoop(updatePosition);
+}
+
+
+function createScene() {
+    var canvas = document.getElementById('canvas');
+    var engine = new BABYLON.Engine(canvas, true); 
+    var scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 20,-15), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+    character = BABYLON.Mesh.CreateSphere('sphere1', 16, 1, scene);
+    character.position.y = 1;
+    var ground = BABYLON.Mesh.CreateGround('ground1', 25, 18, 2, scene);
+
+    return [engine, scene];
+}
+
+
+function updatePosition() {
+    var tempMoveSpeed = charMoveSpeed;
+    if ((keys.left || keys.right) && (keys.up || keys.down)) {
+        tempMoveSpeed /= 1.5;
     }
-    
-    function onKeyUp(event) {  
-        if (event.keyCode == 65 || event.keyCode == 37) { 
-            keys.left = false;
-        }
-        if (event.keyCode == 68 || event.keyCode == 39) {
-            keys.right = false;    
-        }
-        if (event.keyCode == 87 || event.keyCode == 38) {
-            keys.up = false;  
-        }
-        if (event.keyCode == 83 || event.keyCode == 40) {  
-            keys.down = false;
-        } 
+    if (keys.left) {
+        character.position.x -= tempMoveSpeed;
     }
-    
-    engine.runRenderLoop(function () {   
-        var tempMoveSpeed = charMoveSpeed;
-        if ((keys.left || keys.right) && (keys.up || keys.down)) {
-            tempMoveSpeed /= 1.5;
-        }
-        if (keys.left) {
-            character.position.x -= tempMoveSpeed;
-        }
-        if (keys.right) {
-            character.position.x += tempMoveSpeed;
-        }
-        if (keys.up) {
-            character.position.z += tempMoveSpeed;
-        }
-        if (keys.down) {
-            character.position.z -= tempMoveSpeed;
-        }
-    });
-});
+    if (keys.right) {
+        character.position.x += tempMoveSpeed;
+    }
+    if (keys.up) {
+        character.position.z += tempMoveSpeed;
+    }
+    if (keys.down) {
+        character.position.z -= tempMoveSpeed;
+    }
+}
+
+
+function key_up_or_down(event, bool_value) {
+    if (event.keyCode == 65 || event.keyCode == 37) {  
+        keys.left = bool_value;
+    }
+    if (event.keyCode == 68 || event.keyCode == 39) { 
+        keys.right = bool_value;    
+    }
+    if (event.keyCode == 87 || event.keyCode == 38) {
+        keys.up = bool_value;  
+    }
+    if (event.keyCode == 83 || event.keyCode == 40) { 
+        keys.down = bool_value;
+    } 
+}
